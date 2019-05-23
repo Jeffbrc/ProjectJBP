0fa0485f10163042cd469b10295c0127903fa3e8
Paul <eleve@STAEL-109-12>
2019-05-23 16:34:49 +0200

    birun 23.5.19 LAST

--- conf/schema.sql
+++ conf/schema.sql
@@ -7,63 +7,31 @@ CREATE TABLE user
 	email VARCHAR(128) DeFAULT NULL
 ); 
 
-
-INSERT INTO user (username, passwd_hash, first_name,last_name, email) VALUES ('paul','8aefb06c426e07a0a671a1e2488b4858d694a730','Paul','Sarasin','paulsarasin@gmail.com');
-INSERT INTO user (username, passwd_hash, first_name,last_name, email) VALUES ('jefferson','75d472285ec7d59be0927446a22daf2db60ee617','Jefferson','Mudry','mudryjefferson@gmail.com');
-INSERT INTO user (username, passwd_hash, first_name,last_name, email) VALUES ('birun','e7d537e128158790157ea057bb883e0292a84930','Birunthaban','Sivalingam','birun2020@gmail.com');
-
--- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2019-03-24 11:37:44.286
-
 -- tables
 -- Table: brand
 CREATE TABLE brand (
-    id integer NOT NULL CONSTRAINT brand_pk PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     name integer NOT NULL
 );
 
 -- Table: category
 CREATE TABLE category (
-    category_id integer NOT NULL CONSTRAINT category_pk PRIMARY KEY,
-    product_id integer NOT NULL,
-    CONSTRAINT category_product FOREIGN KEY (product_id)
-    REFERENCES product (id)
+    id INTEGER PRIMARY KEY,
+    name integer NOT NULL
 );
 
--- Table: photo
-CREATE TABLE photo (
-    product_photo_product_id integer NOT NULL CONSTRAINT photo_pk PRIMARY KEY,
-    CONSTRAINT photo_product_photo FOREIGN KEY (product_photo_product_id)
-    REFERENCES product_photo (product_id)
-);
 
 -- Table: product
 CREATE TABLE product (
-    id integer NOT NULL CONSTRAINT product_pk PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
+    name varchar(64) NOT NULL,
     brand_id integer NOT NULL,
-    category_id integer NOT NULL,
-    CONSTRAINT product_brand FOREIGN KEY (brand_id)
-    REFERENCES brand (id)
-);
-
--- Table: product_photo
-CREATE TABLE product_photo (
-    product_id integer NOT NULL CONSTRAINT product_photo_pk PRIMARY KEY,
-    CONSTRAINT product_photo_product FOREIGN KEY (product_id)
-    REFERENCES product (id)
-);
-
--- Table: product_size
-CREATE TABLE product_size (
-    product_id integer NOT NULL CONSTRAINT product_size_pk PRIMARY KEY,
-    size_id integer NOT NULL,
-    CONSTRAINT product_size_product FOREIGN KEY (product_id)
-    REFERENCES product (id)
+    category_id integer NOT NULL
 );
 
 -- Table: shop
 CREATE TABLE shop (
-    id integer NOT NULL CONSTRAINT shop_pk PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     name varchar(64) NOT NULL,
     address integer NOT NULL,
     city integer NOT NULL,
@@ -72,22 +40,42 @@ CREATE TABLE shop (
 
 -- Table: shop_product
 CREATE TABLE shop_product (
-    shop_product integer NOT NULL CONSTRAINT shop_product_pk PRIMARY KEY,
     product_id integer NOT NULL,
-    shop_id integer NOT NULL,
-    CONSTRAINT shop_product_product FOREIGN KEY (product_id)
-    REFERENCES product (id),
-    CONSTRAINT shop_product_shop FOREIGN KEY (shop_id)
-    REFERENCES shop (id)
-);
-
--- Table: size
-CREATE TABLE size (
-    id integer NOT NULL CONSTRAINT id PRIMARY KEY,
-    product_size_product_id integer NOT NULL,
-    CONSTRAINT size_product_size FOREIGN KEY (product_size_product_id)
-    REFERENCES product_size (product_id)
+    shop_id integer NOT NULL
 );
 
 -- End of file.
 
+INSERT INTO user (username, passwd_hash, first_name,last_name, email) VALUES ('paul','8aefb06c426e07a0a671a1e2488b4858d694a730','Paul','Sarasin','paulsarasin@gmail.com');
+INSERT INTO user (username, passwd_hash, first_name,last_name, email) VALUES ('jefferson','75d472285ec7d59be0927446a22daf2db60ee617','Jefferson','Mudry','mudryjefferson@gmail.com');
+INSERT INTO user (username, passwd_hash, first_name,last_name, email) VALUES ('birun','e7d537e128158790157ea057bb883e0292a84930','Birunthaban','Sivalingam','birun2020@gmail.com');
+
+-- brands
+INSERT INTO brand (name) VALUES ('adidas');
+INSERT INTO brand (name) VALUES ('fila');
+INSERT INTO brand (name) VALUES ('nike');
+INSERT INTO brand (name) VALUES ('north face');
+INSERT INTO brand (name) VALUES ('vans');
+INSERT INTO brand (name) VALUES ('champion');
+INSERT INTO brand (name) VALUES ('stüssy');
+
+
+-- category
+INSERT INTO category (name) VALUES ('shoes');
+INSERT INTO category (name) VALUES ('t-shirt');
+INSERT INTO category (name) VALUES ('cap');  
+INSERT INTO category (name) VALUES ('hoodie'); 
+INSERT INTO category (name) VALUES ('sweatshirt');
+
+
+-- shops
+INSERT INTO shop (name) VALUES ('Foot Locker');
+INSERT INTO shop (name) VALUES ('Snipes');
+INSERT INTO shop (name) VALUES ('Forexx store');
+INSERT INTO shop (name) VALUES ('Doodah');
+INSERT INTO shop (name) VALUES ('Maniak');
+INSERT INTO shop (name) VALUES ('Pump It Up');
+INSERT INTO shop (name) VALUES ('Blue Tomato');
+
+INSERT INTO category (name) VALUES ('short'); 
+INSERT INTO category (name) VALUES ('socks');     
